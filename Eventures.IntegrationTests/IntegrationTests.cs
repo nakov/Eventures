eab1cb2754f519fd3ba658781ab94992f0b354c1
Svetlin Nakov <github@nakov.com>
2021-05-20 01:20:29 +0300

    work in progress: implementing integration tests

---
+++ Eventures.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+
+namespace Eventures.IntegrationTests
+{
+    public class IntegrationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_UserRegistration()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_UserLogin()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_UserLogout()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_HomePage_AnnonymousUser()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_HomePage_LoggedIn()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_EventsPage_ViewAllEvents()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_EventsPage_CreateEvent_ValidData()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_EventsPage_CreateEvent_InvalidData()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_EventsPage_DeleteEvent()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Eventures.IntegrationTests/TestData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using Eventures.App.Data;
+
+namespace Eventures.IntegrationTests
+{
+    public class TestData
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+        public EventuresUser UserMaria { get; private set; }
+        public Event EventSoftuniada { get; private set; }
+        public Event EventOpenFest { get; private set; }
+
+        public TestData()
+        {
+            // Create an in-memory database for testing
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Eventures-MemoryDb-" + DateTime.Now.Ticks);
+            DbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            this.UserMaria = new EventuresUser()
+            {
+                Id = "25ab6879-32b1-4b44-b0f1-49e85a6418c9",
+                UserName = "maria",
+                Email = "maria@gmail.com",
+                FirstName = "Maria",
+                LastName = "Green",
+            };
+            DbContext.Add(this.UserMaria);
+            this.EventSoftuniada = new Event()
+            {
+                Id = 1,
+                Name = "Softuniada 2021",
+                Place = "Sofia",
+                Start = DateTime.Now.AddMonths(3),
+                End = DateTime.Now.AddMonths(3),
+                TotalTickets = 200,
+                PricePerTicket = 12.00m,
+                OwnerId = UserMaria.Id
+            };
+            DbContext.Add(this.EventSoftuniada);
+            this.EventOpenFest = new Event()
+            {
+                Id = 2,
+                Name = "OpenFest 2021",
+                Place = "Online",
+                Start = DateTime.Now.AddDays(200),
+                End = DateTime.Now.AddDays(201),
+                TotalTickets = 5000,
+                PricePerTicket = 0,
+                OwnerId = UserMaria.Id
+            };
+            DbContext.Add(this.EventOpenFest);
+            DbContext.SaveChanges();
+        }
+
+        public static void AssignCurrentUserForController(
+            Controller controller, EventuresUser user)
+        {
+            var userClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName + " " + user.LastName),
+            };
+            var userIdentity = new ClaimsIdentity(userClaims, "Password");
+            var userPrincipal = new ClaimsPrincipal(userIdentity);
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = userPrincipal }
+            };
+        }
+    }
+}
