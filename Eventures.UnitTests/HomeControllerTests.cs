be39d34b713f9c439abfa37b266b334be7f3d088
evandonova <evelina_ga@abv.bg>
2021-06-10 09:33:05 +0300

    Fixed unit tests bug

--- Eventures.UnitTests/HomeControllerTests.cs
+++ Eventures.UnitTests/HomeControllerTests.cs
@@ -5,16 +5,27 @@ using NUnit.Framework;
 
 using Eventures.App.Controllers;
 using Eventures.App.Models;
+using Eventures.App.Data;
 
 namespace Eventures.UnitTests
 {
     public class HomeControllerTests
     {
+        TestDb testDb;
+        ApplicationDbContext dbContext;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            testDb = new TestDb();
+            dbContext = testDb.CreateDbContext();
+        }
+
         [Test]
         public void Test_Index()
         {
             // Arrange
-            var controller = new HomeController();
+            var controller = new HomeController(dbContext);
 
             // Act
             var result = controller.Index();
@@ -28,7 +39,7 @@ namespace Eventures.UnitTests
         public void Test_Error()
         {
             // Arrange
-            var controller = new HomeController();
+            var controller = new HomeController(dbContext);
             controller.ControllerContext.HttpContext = new DefaultHttpContext();
 
             // Act
