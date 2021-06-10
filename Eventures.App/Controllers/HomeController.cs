b245df3f590100d2fce34f5b557605b775460e19
evandonova <evelina_ga@abv.bg>
2021-06-10 09:19:35 +0300

    Added events' count to Home page

--- Eventures.App/Controllers/HomeController.cs
+++ Eventures.App/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Diagnostics;
-
 using Microsoft.AspNetCore.Mvc;
-
 using Eventures.App.Models;
+using Eventures.App.Data;
 
 namespace Eventures.App.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            this.dbContext = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(this.dbContext.Events);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
