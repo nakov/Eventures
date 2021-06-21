21f4c5087f5d2f38d34e7c5d600e5bdad0d68067
Svetlin Nakov <github@nakov.com>
2021-06-22 02:13:06 +0300

    fix: avoid SSL while testing

--- Eventures.Tests.Common/TestEventuresApp.cs
+++ Eventures.Tests.Common/TestEventuresApp.cs
@@ -42,7 +42,7 @@ namespace Eventures.Tests.Common
             var server = this.host.Services.GetRequiredService<IServer>();
             this.ServerUri =
                 server.Features.Get<IServerAddressesFeature>()
-                .Addresses.LastOrDefault();
+                .Addresses.FirstOrDefault();
         }
 
         public void Dispose()
