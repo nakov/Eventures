84b95486fb2f193b71392d3866ae2f95fa9fe246
evandonova <evelina_ga@abv.bg>
2021-06-22 13:59:57 +0300

    Separated Selenium tests

--- Eventures.SeleniumTests/SeleniumTests_User.cs
+++ Eventures.SeleniumTests/SeleniumTests_User.cs
@@ -16,7 +16,7 @@ namespace Eventures.SeleniumTests
         TestEventuresApp testEventuresApp;
         string baseUrl;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Setup()
         {
             // Run the Web app in a local Web server
@@ -33,7 +33,7 @@ namespace Eventures.SeleniumTests
             this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
-        [Test, Order(1)]
+        [Test]
         public void Test_User_Register()
         {
             // Arrange: go to the "Registration" page
@@ -55,9 +55,10 @@ namespace Eventures.SeleniumTests
             Assert.That(driver.PageSource.Contains($"Welcome, {username}"));
         }
 
-        [Test, Order(2)]
+        [Test]
         public void Test_User_Login()
         {
+            RegisterUser();
             // Arrange: go to the "Login" page
             driver.Navigate().GoToUrl(this.baseUrl + "/Identity/Account/Login");
 
@@ -73,9 +74,10 @@ namespace Eventures.SeleniumTests
             Assert.That(driver.PageSource.Contains($"Welcome, {username}"));
         }
 
-        [Test, Order(3)]
+        [Test]
         public void Test_User_Logout()
         {
+            RegisterUser();
             // Arrange: go to the "Home" page
             driver.Navigate().GoToUrl(this.baseUrl);
 
@@ -87,86 +89,25 @@ namespace Eventures.SeleniumTests
             Assert.That(driver.PageSource.Contains("Eventures: Events and Tickets"));
         }
 
-        [Test]
-        public void Test_HomePage_LoginPageLink_InNavigation()
+        private void RegisterUser()
         {
-            // Arrange: go to the "Home" page
-            driver.Navigate().GoToUrl(this.baseUrl);
+            driver.Navigate().GoToUrl(this.baseUrl + "/Identity/Account/Register");
 
-            // Act: locate and click on [Login] in the navigation bar
-            driver.FindElement(By.XPath("(//a[@href='/Identity/Account/Login'])[1]")).Click();
-            
-            // Assert user is redirected to the "Log in" page
-            Assert.AreEqual(this.baseUrl + "/Identity/Account/Login", driver.Url);
-            Assert.That(driver.Title.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Use a local account to log in"));
+            username = "testuser" + DateTime.UtcNow.Ticks;
+            password = "password" + DateTime.UtcNow.Ticks;
+            driver.FindElement(By.Id("Input_Username")).SendKeys(username);
+            driver.FindElement(By.Id("Input_Email")).SendKeys($"{username}@mail.com");
+            driver.FindElement(By.Id("Input_Password")).SendKeys(password);
+            driver.FindElement(By.Id("Input_ConfirmPassword")).SendKeys(password);
+            driver.FindElement(By.Id("Input_FirstName")).SendKeys("Pesho");
+            driver.FindElement(By.Id("Input_LastName")).SendKeys("Petrov");
+            driver.FindElement(By.XPath("//button[@type='submit'][contains(.,'Register')]")).Click();
+
+            Assert.AreEqual(this.baseUrl + "/", driver.Url);
+            Assert.That(driver.PageSource.Contains($"Welcome, {username}"));
         }
 
-        [Test]
-        public void Test_HomePage_LoginPageLink_OnPage()
-        {
-            // Arrange: go to the "Home" page
-            driver.Navigate().GoToUrl(this.baseUrl);
-
-            // Act: locate and click on [Login] on the main page
-            driver.FindElement(By.XPath("(//a[@href='/Identity/Account/Login'])[2]")).Click();
-
-            // Assert user is redirected to the "Log in" page
-            Assert.AreEqual(this.baseUrl + "/Identity/Account/Login", driver.Url);
-            Assert.That(driver.Title.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Use a local account to log in"));
-        }
-
-        [Test]
-        public void Test_HomePage_RegisterPageLink_InNavigation()
-        {
-            // Arrange: go to the "Home" page
-            driver.Navigate().GoToUrl(this.baseUrl);
-
-            // Act: locate and click on [Register] in the navigation bar
-            driver.FindElement(By.XPath("(//a[@href='/Identity/Account/Register'])[1]")).Click();
-
-            // Assert user is redirected to the "Register" page
-            Assert.AreEqual(this.baseUrl + "/Identity/Account/Register", driver.Url);
-            Assert.That(driver.Title.Contains("Register"));
-            Assert.That(driver.PageSource.Contains("Register"));
-            Assert.That(driver.PageSource.Contains("Create a new account"));
-        }
-
-        [Test]
-        public void Test_HomePage_RegisterPageLink_OnPage()
-        {
-            // Arrange: go to the "Home" page
-            driver.Navigate().GoToUrl(this.baseUrl);
-
-            // Act: locate and click on [Register] on the main page
-            driver.FindElement(By.XPath("(//a[@href='/Identity/Account/Register'])[2]")).Click();
-
-            // Assert user is redirected to the "Register" page
-            Assert.AreEqual(this.baseUrl + "/Identity/Account/Register", driver.Url);
-            Assert.That(driver.Title.Contains("Register"));
-            Assert.That(driver.PageSource.Contains("Register"));
-            Assert.That(driver.PageSource.Contains("Create a new account"));
-        }
-
-        [Test]
-        public void Test_AllEventsPage_Anonymous()
-        {
-            // Arrange
-            // Act: go to the "All Events" page
-
-            driver.Navigate().GoToUrl(this.baseUrl + "/Events/All");
-
-            // Assert user is redirected to the "Log in" page
-            Assert.AreEqual(this.baseUrl + "/Identity/Account/LogIn?ReturnUrl=%2FEvents%2FAll", driver.Url);
-            Assert.That(driver.Title.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Log in"));
-            Assert.That(driver.PageSource.Contains("Use a local account to log in"));
-        }
-
-        [OneTimeTearDown]
+        [TearDown]
         public void TearDown()
         {
             driver.Quit();
