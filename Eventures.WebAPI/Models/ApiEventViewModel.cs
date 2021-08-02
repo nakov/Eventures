21f6e2806bc4542d405bc9f6a6ca9fdc4d6f0b80
evandonova <evelina_ga@abv.bg>
2021-08-02 12:39:55 +0300

    Tried configuration

---
+++ Eventures.WebAPI/Models/ApiEventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eventures.WebAPI.Models
+{
+    public class ApiEventViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int TotalTickets { get; set; }
+
+        [Column(TypeName = "decimal(12,3)")]
+        public decimal PricePerTicket { get; set; }
+
+        public ApiUserViewModel Owner { get; set; }
+    }
+}
---
+++ Eventures.WebAPI/Models/ApiUserViewModel.cs
@@ -0,0 +1,11 @@
+namespace Eventures.WebAPI.Models
+{
+    public class ApiUserViewModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
