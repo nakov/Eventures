b93e2462e8a6aa3f57d97822d988802390876761
evandonova <evelina_ga@abv.bg>
2021-08-05 11:59:42 +0300

    Added sample Appium test

--- Eventures.Desktop.AppiumTests/AppiumTests.cs
+++ Eventures.Desktop.AppiumTests/AppiumTests.cs
@@ -1,24 +1,21 @@
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Service;
 using OpenQA.Selenium.Appium.Windows;
+using System.IO;
 
 namespace Eventures.Desktop.AppiumTests
 {
     public class AppiumTests
     {
         private AppiumLocalService appiumLocalService;
-        private const string AppiumServerUri = "http://[::1]:4723/wd/hub";
+        //private const string AppiumServerUri = "http://[::1]:4723/wd/hub";
         private string AppPath = @"../../../../Eventures.Desktop/bin/Debug/net5.0-windows/Eventures.Desktop.exe";
         private WindowsDriver<WindowsElement> driver;
 
         private string username = "newUser";
         private string eventBoard = "Event Board";
-        private string createEvent = "Create New Event";
+        //private string createEvent = "Create New Event";
 
         [OneTimeSetUp]
         public void Setup()
@@ -46,79 +43,15 @@ namespace Eventures.Desktop.AppiumTests
 
             var statusTextBox = driver.FindElementByXPath(
                 "/Window/StatusBar/Text");
-            string text = statusTextBox.Text;
-            Debug.WriteLine(text);
-            string text2 = statusTextBox.TagName;
-            Debug.WriteLine(text2);
+            Assert.AreEqual("Connected to the Web API.", statusTextBox.Text);
         }
-
-        //[Test]
-        //public void Test_CreateEvent_ValidData()
-        //{
-        //    var createBtn = driver.FindElementByAccessibilityId("buttonCreate");
-        //    createBtn.Click();
-
-        //    Assert.That(driver.PageSource.Contains(this.createEvent));
-
-        //    var eventName = "Fun Event" + DateTime.Now.Ticks;
-        //    var nameField = driver.FindElementByAccessibilityId("textBoxName");
-        //    nameField.Clear();
-        //    nameField.SendKeys(eventName);
-
-        //    var eventPlace = "Beach";
-        //    var placeField = driver.FindElementByAccessibilityId("textBoxPlace");
-        //    placeField.Clear();
-        //    placeField.SendKeys(eventPlace);
-
-        //    var upBtns = driver.FindElementsByName("Up");
-        //    var priceUpBtn = upBtns[0];
-        //    priceUpBtn.Click();
-        //    priceUpBtn.Click();
-
-        //    var ticketsUpBtn = upBtns[1];
-        //    ticketsUpBtn.Click();
-
-        //    var createConfirmationBtn = driver
-        //        .FindElementByAccessibilityId("buttonCreateConfirm");
-        //    createConfirmationBtn.Click();
-
-        //    Assert.That(!driver.PageSource.Contains(this.createEvent));
-        //    Assert.That(driver.PageSource.Contains(this.eventBoard));
-        //    Assert.That(driver.PageSource.Contains(eventName));
-        //    Assert.That(driver.PageSource.Contains(eventPlace));
-        //    Assert.That(driver.PageSource.Contains(this.username));
-        //}
-
-        //[Test]
-        //public void Test_CreateEvent_InvalidData()
-        //{
-        //    var createBtn = driver.FindElementByAccessibilityId("buttonCreate");
-        //    createBtn.Click();
-
-        //    Assert.That(driver.PageSource.Contains(this.createEvent));
-
-        //    var eventName = "Fun Event" + DateTime.Now.Ticks;
-        //    var nameField = driver.FindElementByAccessibilityId("textBoxName");
-        //    nameField.Clear();
-        //    nameField.SendKeys(eventName);
-
-        //    var invalidEventPlace = string.Empty;
-        //    var placeField = driver.FindElementByAccessibilityId("textBoxPlace");
-        //    placeField.Clear();
-        //    placeField.SendKeys(invalidEventPlace);
-
-        //    var createConfirmationBtn = driver.FindElementByAccessibilityId("buttonCreateConfirm");
-        //    createConfirmationBtn.Click();
-
-        //    Assert.That(!driver.PageSource.Contains(this.createEvent));
-        //    Assert.That(driver.PageSource.Contains(this.eventBoard));
-        //    Assert.That(!driver.PageSource.Contains(eventName));
-        //}
 
         [OneTimeTearDown]
         public void TearDown()
         {
+            driver.CloseApp();
             driver.Quit();
+            appiumLocalService.Dispose();
         }
 
         private void Connect(WindowsDriver<WindowsElement> driver)
