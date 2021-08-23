59b901a340370516a3de9037d4732cc7c28f894b
evandonova <evelina_ga@abv.bg>
2021-08-23 17:14:37 +0300

    Added Appium Android tests

---
+++ Eventures.AndroidApp.AppiumTests/AppiumTests.cs
@@ -0,0 +1,224 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Eventures.AndroidApp.AppiumTests
+{
+    public class AppiumTests : AppiumTestsBase
+    {
+        private string username = "newUser" + DateTime.UtcNow.Ticks;
+        private string password = "newPassword12";
+
+        [Test, Order(1)]
+        public void Test_Connect_WithInvalidUrl()
+        {
+            var loginBtn = driver.FindElementById("buttonLogin");
+            Assert.That(loginBtn.Enabled == false);
+            var registerBtn = driver.FindElementById("buttonRegister");
+            Assert.That(registerBtn.Enabled == false);
+
+            var connectBtn = driver.FindElementById("buttonConnect");
+            connectBtn.Click();
+
+            var apiUrlField = driver.FindElementById("editTextApiUrl");
+            apiUrlField.Clear();
+
+            var invalidUrl = "http://10.0.2.2:1234/api/";
+            apiUrlField.SendKeys(invalidUrl);
+
+            var confirmConnectBtn = driver.FindElementById("buttonConfirmConnect");
+            confirmConnectBtn.Click();
+
+            var statusBox = driver.FindElementById("textViewStatus");
+            while (statusBox.Text == "Connecting ...")
+            {
+                statusBox = driver.FindElementById("textViewStatus");
+            }
+
+            Assert.AreEqual("Could not connect. Try again.", statusBox.Text);
+
+            Assert.That(loginBtn.Enabled == false);
+            Assert.That(registerBtn.Enabled == false);
+        }
+
+        [Test, Order(2)]
+        public void Test_Connect_WithValidUrl()
+        {
+            var loginBtn = driver.FindElementById("buttonLogin");
+            Assert.That(loginBtn.Enabled == false);
+            var registerBtn = driver.FindElementById("buttonRegister");
+            Assert.That(registerBtn.Enabled == false);
+
+            var connectBtn = driver.FindElementById("buttonConnect");
+            connectBtn.Click();
+
+            var apiUrlField = driver.FindElementById("editTextApiUrl");
+            apiUrlField.Clear();
+            apiUrlField.SendKeys(@$"{this.baseUrl}/api/");
+
+            var confirmConnectBtn = driver.FindElementById("buttonConfirmConnect");
+            confirmConnectBtn.Click();
+
+            var statusBox = driver.FindElementById("textViewStatus");
+            while (statusBox.Text == "Connecting ...")
+            {
+                statusBox = driver.FindElementById("textViewStatus");
+            }
+
+            Assert.AreEqual("Connected successfully.", statusBox.Text);
+
+            Assert.That(loginBtn.Enabled == true);
+            Assert.That(registerBtn.Enabled == true);
+        }
+
+        [Test, Order(3)]
+        public void Test_Register()
+        {
+            var addBtn = driver.FindElementById("buttonAdd");
+            Assert.That(addBtn.Enabled == false);
+
+            var reloadBtn = driver.FindElementById("buttonReload");
+            Assert.That(reloadBtn.Enabled == false);
+
+            var registerBtn = driver.FindElementById("buttonRegister");
+            Assert.That(registerBtn.Enabled == true);
+            registerBtn.Click();
+
+            var usernameField = driver.FindElementById("editTextUsername");
+            usernameField.Clear();
+            usernameField.SendKeys(this.username);
+
+            var emailField = driver.FindElementById("editTextEmail");
+            emailField.Clear();
+            emailField.SendKeys(this.username + "@mail.com");
+
+            var passwordField = driver.FindElementById("editTextPassword");
+            passwordField.Clear();
+            passwordField.SendKeys(this.password);
+
+            var confirmPasswordField = driver
+                .FindElementById("editTextConfirmPassword");
+            confirmPasswordField.Clear();
+            confirmPasswordField.SendKeys(this.password);
+
+            var firstNameField = driver.FindElementById("editTextFirstName");
+            firstNameField.Clear();
+            firstNameField.SendKeys("Test");
+
+            var lastNameField = driver.FindElementById("editTextLastName");
+            lastNameField.Clear();
+            lastNameField.SendKeys("User");
+
+            var confirmRegisterBtn = driver.FindElementById("buttonConfirmRegister");
+            confirmRegisterBtn.Click();
+
+            var statusBox = driver.FindElementById("textViewStatus");
+            while (!statusBox.Text.Contains("Events found:"))
+            {
+                statusBox = driver.FindElementById("textViewStatus");
+            }
+
+            var eventsInDb = this.dbContext.Events.Count();
+            Assert.That(statusBox.Text.Contains($"Events found: {eventsInDb}"));
+
+            Assert.That(addBtn.Enabled == true);
+            Assert.That(reloadBtn.Enabled == true);
+        }
+
+        [Test]
+        public void Test_Login()
+        {
+            var loginBtn = driver.FindElementById("buttonLogin");
+            Assert.That(loginBtn.Enabled == true);
+            loginBtn.Click();
+
+            var usernameField = driver.FindElementById("editTextUsername");
+            usernameField.Clear();
+            usernameField.SendKeys(this.username);
+
+            var passwordField = driver.FindElementById("editTextPassword");
+            passwordField.Clear();
+            passwordField.SendKeys(this.password);
+
+            var confirmLoginBtn = driver.FindElementById("buttonConfirmLogin");
+            confirmLoginBtn.Click();
+
+            var statusBox = driver.FindElementById("textViewStatus");
+            while (!statusBox.Text.Contains("Events found:"))
+            {
+                statusBox = driver.FindElementById("textViewStatus");
+            }
+
+            var eventsInDb = this.dbContext.Events.Count();
+            Assert.That(statusBox.Text.Contains($"Events found: {eventsInDb}"));
+
+            var addBtn = driver.FindElementById("buttonAdd");
+            Assert.That(addBtn.Enabled == true);
+
+            var reloadBtn = driver.FindElementById("buttonReload");
+            Assert.That(reloadBtn.Enabled == true);
+        }
+
+        [Test]
+        public void Test_CreateEvent_WithValidData()
+        {
+            var eventsInDbBefore = this.dbContext.Events.Count();
+
+            var addBtn = driver.FindElementById("buttonAdd");
+            Assert.That(addBtn.Enabled == true);
+            addBtn.Click();
+
+            var nameField = driver.FindElementById("editTextName");
+            nameField.Clear();
+            nameField.SendKeys("Fun Event" + DateTime.Now.Ticks);
+
+            var placeField = driver.FindElementById("editTextPlace");
+            placeField.Clear();
+            placeField.SendKeys("Beach");
+
+            var startDateField = driver.FindElementById("editTextStartDate");
+            startDateField.Click();
+            var okButton = driver.FindElementByXPath("//android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]");
+            okButton.Click();
+
+            var startTimeField = driver.FindElementById("editTextStartTime");
+            startTimeField.Click();
+            okButton = driver.FindElementByXPath("//android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]");
+            okButton.Click();
+
+            var endDateField = driver.FindElementById("editTextEndDate");
+            endDateField.Click();
+            // Change month to "Sep"
+            var nextMonthBtn = driver.FindElementByXPath("//android.widget.NumberPicker[1]/android.widget.Button[2]");
+            nextMonthBtn.Click();
+            okButton = driver.FindElementByXPath("//android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]");
+            okButton.Click();
+
+            var endTimeField = driver.FindElementById("editTextEndTime");
+            endTimeField.Click();
+            okButton = driver.FindElementByXPath("//android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]");
+            okButton.Click();
+
+            var ticketsField = driver.FindElementById("editTextTickets");
+            ticketsField.Clear();
+            ticketsField.SendKeys("50");
+
+            var priceField = driver.FindElementById("editTextPrice");
+            priceField.Clear();
+            priceField.SendKeys("10.50");
+
+            var createBtn = driver.FindElementById("buttonCreate");
+            createBtn.Click();
+
+            var statusBox = driver.FindElementById("textViewStatus");
+            while (!statusBox.Text.Contains("Events found:"))
+            {
+                statusBox = driver.FindElementById("textViewStatus");
+            }
+
+            var eventsInDbAfter = this.dbContext.Events.Count();
+            Assert.AreEqual(eventsInDbBefore + 1, eventsInDbAfter);
+            Assert.That(statusBox.Text.Contains($"Events found: {eventsInDbAfter}"));
+        }
+    }
+}
---
+++ Eventures.AndroidApp.AppiumTests/AppiumTestsBase.cs
@@ -0,0 +1,63 @@
+using Eventures.Data;
+using Eventures.Tests.Common;
+using Eventures.WebAPI;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+using System;
+using System.IO;
+
+namespace Eventures.AndroidApp.AppiumTests
+{
+    public class AppiumTestsBase
+    {
+        protected TestDb testDb;
+        protected ApplicationDbContext dbContext;
+        private TestEventuresApp<Startup> testEventuresApp;
+        protected string baseUrl;
+        private AppiumLocalService appiumLocalService;
+        private string ApiPath = @"../../../../Eventures.WebAPI";
+        private string AppPath = @"../../../../Eventures.AndroidApp/app/build/outputs/apk/debug/eventures.apk";
+        protected AndroidDriver<AndroidElement> driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUpBase()
+        {
+            this.testDb = new TestDb();
+            this.dbContext = testDb.CreateDbContext();
+            this.testEventuresApp = new TestEventuresApp<Startup>(testDb, ApiPath);
+            this.baseUrl = this.testEventuresApp.ServerUri
+                .Replace("127.0.0.1", "10.0.2.2");
+
+            // Initialize Appium Local Service to start the Appium server automatically
+            appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+            appiumLocalService.Start();
+
+            var appiumOptions = new AppiumOptions() { PlatformName = "Android" };
+            var fullPathName = Path.GetFullPath(AppPath);
+            appiumOptions.AddAdditionalCapability("app", fullPathName);
+
+            // Initialize the Windows driver with Appium local service and options
+            driver = new AndroidDriver<AndroidElement>(
+                appiumLocalService, appiumOptions);
+
+            // Set an implicit wait for the UI interaction
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDownBase()
+        {
+            // Close the app and quit the driver
+            driver.CloseApp();
+            driver.Quit();
+
+            // Dispose of the Appium Local Service
+            appiumLocalService.Dispose();
+
+            // Stop and dispose the local Web API server
+            this.testEventuresApp.Dispose();
+        }
+    }
+}
