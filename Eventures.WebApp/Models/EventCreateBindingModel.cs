7f29f499040d0f62c7e1f5c975131d18012ceddd
evandonova <evelina_ga@abv.bg>
2021-09-22 17:22:36 +0300

    Added restrictions to models

--- Eventures.WebApp/Models/EventCreateBindingModel.cs
+++ Eventures.WebApp/Models/EventCreateBindingModel.cs
@@ -1,34 +1,44 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
+using Eventures.Data;
+using Eventures.Data.Attributes;
+
 namespace Eventures.WebApp.Models
 {
+    using static DataConstants;
     public class EventCreateBindingModel
     {
         [Required]
         [Display(Name = "Name")]
+        [StringLength(MaxEventName, MinimumLength = MinEventName,
+            ErrorMessage = "{0} should be at least {2} characters long.")]
         public string Name { get; set; }
 
         [Required]
         [Display(Name = "Place")]
+        [StringLength(MaxEventPlace, MinimumLength = MinEventPlace,
+            ErrorMessage = "{0} should be at least {2} characters long.")]
         public string Place { get; set; }
 
         [Required]
         [Display(Name = "Start")]
+        [CustomDateValidation]
         public DateTime Start { get; set; }
 
         [Required]
         [Display(Name = "End")]
+        [DateGreaterThan("Start")]
         public DateTime End { get; set; }
 
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Total Tickets must be a positive number.")]
         [Display(Name = "TotalTickets")]
+        [Range(0, MaxEventTickets, ErrorMessage = "Total Tickets must be a positive number and less than {2}.")]
         public int TotalTickets { get; set; }
 
         [Required]
-        [Range(0.00, double.MaxValue, ErrorMessage = "Price Per Ticket must be a positive number.")]
         [Display(Name = "PricePerTicket")]
+        [Range(0.00, MaxEventPrice, ErrorMessage = "Price Per Ticket must be a positive number and less than {2}.")]
         public decimal PricePerTicket { get; set; }
     }
 }
