eab1615429f64afa618a0c7c04cf1bda4530e5e7
evandonova <evelina_ga@abv.bg>
2021-09-22 17:22:55 +0300

    Added restrictions to models

--- Eventures.WebAPI/Models/Event/EventBindingModel.cs
+++ Eventures.WebAPI/Models/Event/EventBindingModel.cs
@@ -1,34 +1,42 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
+using Eventures.Data;
+using Eventures.Data.Attributes;
+
 namespace Eventures.WebAPI.Models.Event
 {
+    using static DataConstants;
     public class EventBindingModel
     {
         [Required]
         [Display(Name = "Name")]
+        [StringLength(MaxEventName, MinimumLength = MinEventName)]
         public string Name { get; init; }
 
         [Required]
         [Display(Name = "Place")]
+        [StringLength(MaxEventPlace, MinimumLength = MinEventPlace)]
         public string Place { get; init; }
 
         [Required]
         [Display(Name = "Start")]
+        [CustomDateValidation]
         public DateTime Start { get; init; }
 
         [Required]
         [Display(Name = "End")]
+        [DateGreaterThan("Start")]
         public DateTime End { get; init; }
 
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Total Tickets must be a positive number.")]
         [Display(Name = "TotalTickets")]
+        [Range(0, MaxEventTickets)]
         public int TotalTickets { get; init; }
 
         [Required]
-        [Range(0.00, double.MaxValue, ErrorMessage = "Price Per Ticket must be a positive number.")]
         [Display(Name = "PricePerTicket")]
+        [Range(0.00, MaxEventPrice)]
         public decimal PricePerTicket { get; init; }
     }
 }
