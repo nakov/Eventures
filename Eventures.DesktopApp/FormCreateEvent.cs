ddc883e8200a1ea65ce828d0e1c99917d82dc3b1
evandonova <evelina_ga@abv.bg>
2021-09-23 12:09:45 +0300

    Fixed bug in datetime parsing

--- Eventures.DesktopApp/FormCreateEvent.cs
+++ Eventures.DesktopApp/FormCreateEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace Eventures.DesktopApp
@@ -7,8 +8,8 @@ namespace Eventures.DesktopApp
     {
         public string EvName { get => this.textBoxName.Text; }
         public string Place { get => this.textBoxPlace.Text; }
-        public DateTime Start { get => DateTime.Parse(this.dateTimePickerStart.Text); }
-        public DateTime End { get => DateTime.Parse(this.dateTimePickerEnd.Text); }
+        public DateTime Start { get => DateTime.ParseExact(this.dateTimePickerStart.Text, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture); }
+        public DateTime End { get => DateTime.ParseExact(this.dateTimePickerEnd.Text, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture); }
         public int TotalTickets { get => int.Parse(this.numboxTickets.Text); }
         public decimal PricePerTicket { get => decimal.Parse(this.numboxPrice.Text); }
 
