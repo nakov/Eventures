9c741e33e488acd548a899c372e02935f25cf0d9
evandonova <evelina_ga@abv.bg>
2021-09-29 15:30:21 +0300

    Added form validations to the Android app

--- Eventures.AndroidApp/app/src/main/java/eventures/ui/AddEventActivity.java
+++ Eventures.AndroidApp/app/src/main/java/eventures/ui/AddEventActivity.java
@@ -3,6 +3,7 @@ package eventures.ui;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,20 +11,27 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.PopupWindow;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventures.android.R;
+import com.google.type.DateTime;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddEventActivity extends AppCompatActivity {
+    EditText editTextName, editTextPlace, editTextStartDate, editTextStartTime, editTextEndDate, editTextEndTime, editTextTickets, editTextPrice;
+    String start, end;
+
     @SuppressLint("ClickableViewAccessibility")
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -31,15 +39,18 @@ public class AddEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        EditText editTextName = findViewById(R.id.editTextName);
+        editTextName = findViewById(R.id.editTextName);
         editTextName.requestFocus();
-        EditText editTextPlace = findViewById(R.id.editTextPlace);
-        EditText editTextStartDate = findViewById(R.id.editTextStartDate);
-        EditText editTextStartTime = findViewById(R.id.editTextStartTime);
-        EditText editTextEndDate = findViewById(R.id.editTextEndDate);
-        EditText editTextEndTime = findViewById(R.id.editTextEndTime);
-        EditText editTextTickets = findViewById(R.id.editTextTickets);
-        EditText editTextPrice = findViewById(R.id.editTextPrice);
+        editTextPlace = findViewById(R.id.editTextPlace);
+
+        editTextStartDate = findViewById(R.id.editTextStartDate);
+        editTextStartTime = findViewById(R.id.editTextStartTime);
+
+        editTextEndDate = findViewById(R.id.editTextEndDate);
+        editTextEndTime = findViewById(R.id.editTextEndTime);
+
+        editTextTickets = findViewById(R.id.editTextTickets);
+        editTextPrice = findViewById(R.id.editTextPrice);
 
         @SuppressLint("ClickableViewAccessibility")
         View.OnTouchListener touchListenerDateSelect = (v, event) -> {
@@ -100,15 +111,172 @@ public class AddEventActivity extends AppCompatActivity {
 
         Button buttonCreate = findViewById(R.id.buttonCreate);
         buttonCreate.setOnClickListener(v -> {
-            Intent resultData = new Intent();
-            resultData.putExtra("name", editTextName.getText().toString());
-            resultData.putExtra("place", editTextPlace.getText().toString());
-            resultData.putExtra("start", editTextStartDate.getText().toString() + "T" + editTextStartTime.getText().toString());
-            resultData.putExtra("end", editTextEndDate.getText().toString() + "T" + editTextEndTime.getText().toString());
-            resultData.putExtra("totalTickets", editTextTickets.getText().toString());
-            resultData.putExtra("pricePerTicket", editTextPrice.getText().toString());
-            setResult(RESULT_OK, resultData);
-            finish();
+            start = editTextStartDate.getText().toString() + "T" + editTextStartTime.getText().toString();
+            end = editTextEndDate.getText().toString() + "T" + editTextEndTime.getText().toString();
+            String errors = CheckAllFields();
+            if(errors == "") {
+                Intent resultData = new Intent();
+                resultData.putExtra("name", editTextName.getText().toString());
+                resultData.putExtra("place", editTextPlace.getText().toString());
+                resultData.putExtra("start", start);
+                resultData.putExtra("end", end);
+                resultData.putExtra("totalTickets", editTextTickets.getText().toString());
+                resultData.putExtra("pricePerTicket", editTextPrice.getText().toString());
+                setResult(RESULT_OK, resultData);
+                finish();
+            }
+            else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(errors)
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Errors");
+                alert.show();
+            }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String CheckAllFields() {
+        StringBuilder errors = new StringBuilder();
+
+        // Name checks
+        String name = editTextName.getText().toString();
+        if (name.length() == 0) {
+            errors.append("Name field is required.");
+        }
+        int minNameLength = 3;
+        if (name.length() > 0 && name.length() < minNameLength) {
+            errors.append(System.lineSeparator());
+            errors.append("Name must be at least " + minNameLength + " characters long.");
+        }
+
+        // Place checks
+        String place = editTextPlace.getText().toString();
+        if (place.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Place field is required.");
+        }
+        int minPlaceLength = 3;
+        if (place.length() > 0 && place.length() < minPlaceLength) {
+            errors.append(System.lineSeparator());
+            errors.append("Place must be at least " + minPlaceLength + " characters long.");
+        }
+
+        // Start date and time checks
+        String startDate = editTextStartDate.getText().toString();
+        if (startDate.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Start Date field is required.");
+        }
+
+        String startTime = editTextStartTime.getText().toString();
+        if (startTime.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Start Time field is required.");
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date startDateTime = new Date();
+        try {
+            startDateTime = sdf.parse(start);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        try {
+            if (startDateTime.before(sdf.parse(String.valueOf(now)))) {
+                errors.append(System.lineSeparator());
+                errors.append("Start Date must be in the future.");
+            }
+            else if(startDateTime.after(sdf.parse("2100-01-01T00:00:00.000"))) {
+                errors.append(System.lineSeparator());
+                errors.append("Start Date must be before the 2100 year.");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // End date and time checks
+        String endDate = editTextEndDate.getText().toString();
+        if (endDate.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("End Date field is required.");
+        }
+
+        String endTime = editTextEndTime.getText().toString();
+        if (endTime.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("End Time field is required.");
+        }
+
+        Date endDateTime = new Date();
+        try {
+            endDateTime = sdf.parse(end);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (endDateTime != null && endDateTime.before(startDateTime)) {
+            errors.append(System.lineSeparator());
+            errors.append("End Date must be after the Start Date.");
+        }
+        else {
+            try {
+                if(endDateTime != null &&  endDateTime.after(sdf.parse("2100-01-01T00:00:00.000"))) {
+                    errors.append(System.lineSeparator());
+                    errors.append("End Date must be before the 2100 year.");
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Tickets checks
+        String ticketsStr = editTextTickets.getText().toString();
+        if (ticketsStr.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Tickets field is required.");
+        }
+        else {
+            int tickets = Integer.parseInt(editTextTickets.getText().toString());
+            if (tickets < 0) {
+                errors.append(System.lineSeparator());
+                errors.append("Tickets must be a positive number.");
+            }
+            int maxTickets = 1000;
+            if (tickets > maxTickets) {
+                errors.append(System.lineSeparator());
+                errors.append("Tickets must be less than " + maxTickets + ".");
+            }
+        }
+
+        // Price checks
+        String priceStr = editTextPrice.getText().toString();
+        if (priceStr.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Price field is required.");
+        }
+        else {
+            double price = Double.parseDouble(editTextPrice.getText().toString());
+            if (price < 0) {
+                errors.append(System.lineSeparator());
+                errors.append("Price must be a positive number.");
+            }
+            int maxPrice = 1000;
+            if (price > maxPrice) {
+                errors.append(System.lineSeparator());
+                errors.append("Price must be less than " + maxPrice + ".");
+            }
+        }
+
+        return  errors.toString();
     }
 }
--- Eventures.AndroidApp/app/src/main/java/eventures/ui/RegisterActivity.java
+++ Eventures.AndroidApp/app/src/main/java/eventures/ui/RegisterActivity.java
@@ -1,29 +1,39 @@
 package eventures.ui;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventures.android.R;
 
+import java.util.Objects;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 public class RegisterActivity extends AppCompatActivity {
+    EditText editTextUserName, editTextEmail, editTextPassword, editTextConfirmPassword, editTextFirstName, editTextLastName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText editTextUserName = findViewById(R.id.editTextUsername);
+        editTextUserName = findViewById(R.id.editTextUsername);
         editTextUserName.requestFocus();
-        EditText editTextEmail = findViewById(R.id.editTextEmail);
-        EditText editTextPassword = findViewById(R.id.editTextPassword);
-        EditText editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
-        EditText editTextFirstName = findViewById(R.id.editTextFirstName);
-        EditText editTextLastName = findViewById(R.id.editTextLastName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
 
         Button buttonCancel = findViewById(R.id.buttonCancel);
         buttonCancel.setOnClickListener(v -> {
@@ -33,15 +43,92 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button buttonLogin = findViewById(R.id.buttonConfirmRegister);
         buttonLogin.setOnClickListener(v -> {
-            Intent resultData = new Intent();
-            resultData.putExtra("username", editTextUserName.getText().toString());
-            resultData.putExtra("email", editTextEmail.getText().toString());
-            resultData.putExtra("password", editTextPassword.getText().toString());
-            resultData.putExtra("confirmPassword", editTextConfirmPassword.getText().toString());
-            resultData.putExtra("firstName", editTextFirstName.getText().toString());
-            resultData.putExtra("lastName", editTextLastName.getText().toString());
-            setResult(RESULT_OK, resultData);
-            finish();
+            String errors = CheckAllFields();
+            if(errors == "") {
+                Intent resultData = new Intent();
+                resultData.putExtra("username", editTextUserName.getText().toString());
+                resultData.putExtra("email", editTextEmail.getText().toString());
+                resultData.putExtra("password", editTextPassword.getText().toString());
+                resultData.putExtra("confirmPassword", editTextConfirmPassword.getText().toString());
+                resultData.putExtra("firstName", editTextFirstName.getText().toString());
+                resultData.putExtra("lastName", editTextLastName.getText().toString());
+                setResult(RESULT_OK, resultData);
+                finish();
+            }
+            else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(errors)
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Errors");
+                alert.show();
+            }
         });
+    }
+
+    private String CheckAllFields() {
+        StringBuilder errors = new StringBuilder();
+        String username = editTextUserName.getText().toString();
+        if (username.length() == 0) {
+            errors.append("Username field is required.");
+        }
+
+        String email = editTextEmail.getText().toString();
+        if (email.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Email field is required.");
+        }
+        else {
+            try {
+                InternetAddress emailAddr = new InternetAddress(email);
+                emailAddr.validate();
+            } catch (AddressException ex) {
+                errors.append(System.lineSeparator());
+                errors.append("Email field must have a valid email address.");
+            }
+        }
+
+        String firstName = editTextFirstName.getText().toString();
+        if (firstName.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("First Name field is required.");
+        }
+
+        String lastName = editTextLastName.getText().toString();
+        if (lastName.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Last Name field is required.");
+        }
+
+        String password = editTextPassword.getText().toString();
+        if (password.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Password field is required.");
+        }
+
+        int minPassLength = 6;
+        if (password.length() > 0 && password.length() < minPassLength) {
+            errors.append(System.lineSeparator());
+            errors.append("Password must be at least " + minPassLength + " characters long.");
+        }
+
+        String confirmPassword = editTextConfirmPassword.getText().toString();
+        if (confirmPassword.length() == 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Confirm Password field is required.");
+        }
+
+        if(confirmPassword.length() > 0 && confirmPassword.compareTo(password) != 0) {
+            errors.append(System.lineSeparator());
+            errors.append("Confirm Password and Password don't match.");
+        }
+
+        return  errors.toString();
     }
 }
